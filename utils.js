d1d8c62079579f6731978bcde88381b60451862b
Javoy Spence <javoyspence@gmail.com>
2024-05-10 21:13:35 -0500

    commit

--- utils.js
+++ utils.js
@@ -10,14 +10,24 @@ export const getRandomHexValues = (len) => {
 
 
 
-export function sendSMS(toNumber, messageBody) {
-    
+const accountSid = 'AC7334186d3844775252e438d61e62fbc3'; 
+const authToken = 'c0a3f9fbe8d79d02313c20ddf1399226'; 
+const client = twilio(accountSid, authToken);
+
+export function sendReminderSMS(toNumber, messageBody, appointmentDate) {
+    const reminderDate = new Date(appointmentDate);
+    reminderDate.setDate(reminderDate.getDate() - 1);
+    reminderDate.setHours(10, 0, 0, 0);
+
     client.messages.create({
         from: '+12177591748', 
         to: toNumber,
         body: messageBody,
+        sendAt: reminderDate.toISOString(),
     })
-    .then(message => console.log(`Message sent successfully with SID: ${message.sid}`))
-    .catch(error => console.error('Error sending message:', error));
+    .then(message => console.log(`Reminder SMS scheduled successfully with SID: ${message.sid}`))
+    .catch(error => console.error('Error scheduling reminder SMS:', error));
 }
+
+
 
